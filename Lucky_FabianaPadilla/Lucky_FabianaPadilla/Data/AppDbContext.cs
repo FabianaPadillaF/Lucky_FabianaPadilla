85fc6eb5515b541254ea1559be91d6dba288694b
Fabiana <a2020115603@estudiantes.upsa.edu.bo>
2021-10-13 22:57:04 -0400

    Final Commit

---
+++ Lucky_FabianaPadilla/Lucky_FabianaPadilla/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Lucky_FabianaPadilla.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lucky_FabianaPadilla.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // Agregar los modelos a utilizar
+        public DbSet<Suerte> Lucky_FabianaPadilla { get; set; }
+    }
+}
+
